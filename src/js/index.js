d5abf445d79ad32805d11155d287d6ca4b95cf2c
kauamassei <gameplayultra11@gmail.com>
2025-03-20 21:43:18 -0300

    feat: add checkAnswer and  nextQuestion functions

--- src/js/index.js
+++ src/js/index.js
@@ -7,122 +7,154 @@ let points = 0;
 let actualQuestion = 0;
 
 const questions = [
-    {
-      "question": "PHP foi desenvolvido para qual fim?",
-      "answers": [
-        {
-          "answer": "back-end",
-          "correct": true
-        },
-        {
-          "answer": "front-end",
-          "correct": false
-        },
-        {
-          "answer": "Sistema operacional",
-          "correct": false
-        },
-        {
-          "answer": "Banco de dados",
-          "correct": false
-        },
-      ]
-    },
-    {
-      "question": "Uma forma de declarar variável em JavaScript:",
-      "answers": [
-        {
-          "answer": "$var",
-          "correct": false
-        },
-        {
-          "answer": "var",
-          "correct": true
-        },
-        {
-          "answer": "@var",
-          "correct": false
-        },
-        {
-          "answer": "#let",
-          "correct": false
-        },
-      ]
-    },
-    {
-      "question": "Qual o seletor de id no CSS?",
-      "answers": [
-        {
-          "answer": "#",
-          "correct": true
-        },
-        {
-          "answer": ".",
-          "correct": false
-        },
-        {
-          "answer": "@",
-          "correct": false
-        },
-        {
-          "answer": "/",
-          "correct": false
-        },
-      ]
-    },
-  ];
+  {
+    question: "PHP foi desenvolvido para qual fim?",
+    answers: [
+      {
+        answer: "back-end",
+        correct: true,
+      },
+      {
+        answer: "front-end",
+        correct: false,
+      },
+      {
+        answer: "Sistema operacional",
+        correct: false,
+      },
+      {
+        answer: "Banco de dados",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Uma forma de declarar variável em JavaScript:",
+    answers: [
+      {
+        answer: "$var",
+        correct: false,
+      },
+      {
+        answer: "var",
+        correct: true,
+      },
+      {
+        answer: "@var",
+        correct: false,
+      },
+      {
+        answer: "#let",
+        correct: false,
+      },
+    ],
+  },
+  {
+    question: "Qual o seletor de id no CSS?",
+    answers: [
+      {
+        answer: "#",
+        correct: true,
+      },
+      {
+        answer: ".",
+        correct: false,
+      },
+      {
+        answer: "@",
+        correct: false,
+      },
+      {
+        answer: "/",
+        correct: false,
+      },
+    ],
+  },
+];
 
 const init = () => {
-    createQuestion(0)
-}
+  createQuestion(0);
+};
 
 const createQuestion = (i) => {
+  // limpando a questão anterior
+  const oldButtons = answersBox.querySelectorAll("button");
+
+  oldButtons.forEach(function (btn) {
+    btn.remove();
+  });
+
+  // alterando texto da pergunta
+  const questionText = question.querySelector("#question-text");
+  const questionNumber = question.querySelector("#question-number");
+
+  questionText.textContent = questions[i].question;
+  questionNumber.textContent = i + 1;
+
+  // insere as alternativas
+  questions[i].answers.forEach(function (answer, i) {
+    // cria o template do botão quizz
+    const answerTemplate = document
+      .querySelector(".answer-template")
+      .cloneNode(true);
+
+    const letterBtn = answerTemplate.querySelector(".btn-letter");
+    const answerText = answerTemplate.querySelector(".question-answer");
+
+    letterBtn.textContent = letters[i];
+    answerText.textContent = answer["answer"];
+
+    answerTemplate.setAttribute("correct-answer", answer["correct"]);
+
+    // removendo hide e template class
+    answerTemplate.classList.remove("hide");
+    answerTemplate.classList.remove("answer-template");
+
+    // inserir a alternativa na tela
+
+    answersBox.appendChild(answerTemplate);
+
+    // inserir um evento de click no botão
+    answerTemplate.addEventListener("click", function () {
+      checkAnswer(this);
+    });
+  });
+
+  // incrementando numero da questão
+  actualQuestion++;
+};
+
+const checkAnswer = (btn) => {
+  const buttons = answersBox.querySelectorAll("button");
+
+  // verifica se a resposta esta correta e adiciona classes botoes
+  buttons.forEach(function (button) {
+    if (button.getAttribute("correct-answer") === "true") {
+      button.classList.add("correct-answer");
+
+      if (btn === button) {
+        points++;
+      }
+    } else {
+      button.classList.add("wrong-answer");
+    }
+  });
+
+  nextQuestion();
+};
+
+const nextQuestion = () => {
+  setTimeout(function () {
+    // verifica se ainda há perguntas
+    if (actualQuestion >= questions.length) {
+        //apresenta a msg de sucesso
+    }
+
+    createQuestion(actualQuestion);
+
     
-    // limpando a questão anterior
-    const oldButtons = answersBox.querySelectorAll("button");
-
-    oldButtons.forEach(function(btn) {
-        btn.remove()
-    });
-
-    // alterando texto da pergunta
-    const questionText = question.querySelector("#question-text");
-    const questionNumber = question.querySelector("#question-number");
-
-    questionText.textContent = questions[i].question;
-    questionNumber.textContent = i + 1;
-
-    // insere as alternativas
-    questions[i].answers.forEach(function(answer, i) {
-
-        // cria o template do botão quizz
-        const answerTemplate = document.querySelector(".answer-template").cloneNode(true);
-
-        const letterBtn = answerTemplate.querySelector(".btn-letter");
-        const answerText = answerTemplate.querySelector(".question-answer");
-
-        letterBtn.textContent = letters[i];
-        answerText.textContent = answer['answer'];
-
-        answerTemplate.setAttribute("correct-answer", answer["correct"]);
-
-        // remocendo hide e template class
-        answerTemplate.classList.remove("hide");
-        answerTemplate.classList.remove("answer-template")
-
-        // inserir a alternativa na tela
-
-        answersBox.appendChild(answerTemplate);
-
-        // inserir um evento de click no botão
-        answerTemplate.addEventListener("click", function() {
-            console.log(this)
-        });
-    });
-
-    // incrementando numero da questão
-    actualQuestion++;
+  }, 1500);
 };
 
 //
-init()
+init();
